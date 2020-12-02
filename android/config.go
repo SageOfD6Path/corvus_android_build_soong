d29d859dd910bc844785d7bb7d89fe4c3a3bd2f5
Ritzz <riteshm321@gmail.com>
2020-12-02 20:35:51 +0100

    Revert "soong: Add function to return camera parameters library name"
    This reverts commit 7cef4d758bf74ca6f064e17670e32c73af704495.

--- android/config.go
+++ android/config.go
@@ -1066,10 +1066,6 @@ func (c *deviceConfig) JavaCoverageEnabledForPath(path string) bool {
 	return coverage
 }
 
-func (c *deviceConfig) SpecificCameraParametersLibrary() string {
-	return String(c.config.productVariables.Corvus.Specific_camera_parameter_library)
-}
-
 // Returns true if gcov or clang coverage is enabled.
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.productVariables.GcovCoverage) ||
