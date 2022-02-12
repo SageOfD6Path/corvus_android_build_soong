omnirom/android_build_soong

soong: add support for building product overlay files
add product_overlay_specific: true to bp file to install files
into /prodict/vendor_overlay/<version>

Change-Id: I38c25a99c6a9aeee7686881427dff88cba8feb1c

android/module.go
@@ -74,6 +74,7 @@ type androidBaseContext interface {
 	SocSpecific() bool
 	ProductSpecific() bool
 	ProductServicesSpecific() bool
+	ProductOverlaySpecific() bool
 	AConfig() Config
 	DeviceConfig() DeviceConfig
 }
@@ -259,6 +260,9 @@ type commonProperties struct {
 	// product_services partition does not exist).
 	Product_services_specific *bool
 
+	// When set to true, it is installed into  /product/vendor_overlay
+	Product_overlay_specific *bool
+
 	// Whether this module is installed to recovery partition
 	Recovery *bool
 
@@ -358,6 +362,7 @@ const (
 	socSpecificModule
 	productSpecificModule
 	productServicesSpecificModule
+	productOverlaySpecificModule
 )
 
 func (k moduleKind) String() string {
@@ -372,7 +377,9 @@ func (k moduleKind) String() string {
 		return "product-specific"
 	case productServicesSpecificModule:
 		return "productservices-specific"
-	default:
+	case productOverlaySpecificModule:
+		return "productoverlay-specific"
+    default:
 		panic(fmt.Errorf("unknown module kind %d", k))
 	}
 }
@@ -597,7 +604,7 @@ func (a *ModuleBase) DeviceSupported() bool {
 }
 
 func (a *ModuleBase) Platform() bool {
-	return !a.DeviceSpecific() && !a.SocSpecific() && !a.ProductSpecific() && !a.ProductServicesSpecific()
+	return !a.DeviceSpecific() && !a.SocSpecific() && !a.ProductSpecific() && !a.ProductServicesSpecific() && !a.ProductOverlaySpecific()
 }
 
 func (a *ModuleBase) DeviceSpecific() bool {
@@ -614,6 +621,10 @@ func (a *ModuleBase) ProductSpecific() bool {
 
 func (a *ModuleBase) ProductServicesSpecific() bool {
 	return Bool(a.commonProperties.Product_services_specific)
+}
+
+func (a *ModuleBase) ProductOverlaySpecific() bool {
+	return Bool(a.commonProperties.Product_overlay_specific)
 }
 
 func (a *ModuleBase) Enabled() bool {
@@ -739,6 +750,7 @@ func determineModuleKind(a *ModuleBase, ctx blueprint.BaseModuleContext) moduleK
 	var deviceSpecific = Bool(a.commonProperties.Device_specific)
 	var productSpecific = Bool(a.commonProperties.Product_specific)
 	var productServicesSpecific = Bool(a.commonProperties.Product_services_specific)
+	var productOverlaySpecific = Bool(a.commonProperties.Product_overlay_specific)
 
 	msg := "conflicting value set here"
 	if socSpecific && deviceSpecific {
@@ -754,12 +766,12 @@ func determineModuleKind(a *ModuleBase, ctx blueprint.BaseModuleContext) moduleK
 		}
 	}
 
-	if productSpecific && productServicesSpecific {
+	if productSpecific && (productServicesSpecific || productOverlaySpecific) {
 		ctx.PropertyErrorf("product_specific", "a module cannot be specific to product and product_services at the same time.")
 		ctx.PropertyErrorf("product_services_specific", msg)
 	}
 
-	if (socSpecific || deviceSpecific) && (productSpecific || productServicesSpecific) {
+	if (socSpecific || deviceSpecific) && (productSpecific || productServicesSpecific || productOverlaySpecific) {
 		if productSpecific {
 			ctx.PropertyErrorf("product_specific", "a module cannot be specific to SoC or device and product at the same time.")
 		} else {
@@ -784,7 +796,9 @@ func determineModuleKind(a *ModuleBase, ctx blueprint.BaseModuleContext) moduleK
 		return productSpecificModule
 	} else if productServicesSpecific {
 		return productServicesSpecificModule
-	} else if deviceSpecific {
+	} else if productOverlaySpecific {
+		return productOverlaySpecificModule
+    } else if deviceSpecific {
 		return deviceSpecificModule
 	} else if socSpecific {
 		return socSpecificModule
@@ -1244,6 +1258,10 @@ func (a *androidBaseContextImpl) ProductServicesSpecific() bool {
 	return a.kind == productServicesSpecificModule
 }
 
+func (a *androidBaseContextImpl) ProductOverlaySpecific() bool {
+	return a.kind == productOverlaySpecificModule
+}
+
 // Makes this module a platform module, i.e. not specific to soc, device,
 // product, or product_services.
 func (a *ModuleBase) MakeAsPlatform() {
@@ -1252,6 +1270,7 @@ func (a *ModuleBase) MakeAsPlatform() {
 	a.commonProperties.Soc_specific = boolPtr(false)
 	a.commonProperties.Product_specific = boolPtr(false)
 	a.commonProperties.Product_services_specific = boolPtr(false)
+	a.commonProperties.Product_overlay_specific = boolPtr(false)
 }
 
 func (a *androidModuleContext) InstallInData() bool {

SageOfD6Path/corvus_android_build_soong

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

android/module.go
@@ -108,6 +108,7 @@ type EarlyModuleContext interface {
 	SocSpecific() bool
 	ProductSpecific() bool
 	SystemExtSpecific() bool
+        ProductOverlaySpecific() bool
 	Platform() bool
 
 	Config() Config
@@ -753,6 +754,9 @@ type commonProperties struct {
 	// (or /system/system_ext if system_ext partition does not exist).
 	System_ext_specific *bool
 
+	// When set to true, it is installed into  /product/vendor_overlay
+	Product_overlay_specific *bool
+
 	// Whether this module is installed to recovery partition
 	Recovery *bool
 
@@ -983,6 +987,7 @@ const (
 	socSpecificModule
 	productSpecificModule
 	systemExtSpecificModule
+	productOverlaySpecificModule
 )
 
 func (k moduleKind) String() string {
@@ -997,6 +1002,8 @@ func (k moduleKind) String() string {
 		return "product-specific"
 	case systemExtSpecificModule:
 		return "systemext-specific"
+	case productOverlaySpecificModule:
+		return "productoverlay-specific"
 	default:
 		panic(fmt.Errorf("unknown module kind %d", k))
 	}
@@ -1384,7 +1391,7 @@ func (m *ModuleBase) HostCrossSupported() bool {
 }
 
 func (m *ModuleBase) Platform() bool {
-	return !m.DeviceSpecific() && !m.SocSpecific() && !m.ProductSpecific() && !m.SystemExtSpecific()
+	return !m.DeviceSpecific() && !m.SocSpecific() && !m.ProductSpecific() && !m.SystemExtSpecific() && !m.ProductOverlaySpecific()
 }
 
 func (m *ModuleBase) DeviceSpecific() bool {
@@ -1440,8 +1447,12 @@ func (m *ModuleBase) PartitionTag(config DeviceConfig) string {
 		if config.SystemExtPath() == "system_ext" {
 			partition = "system_ext"
 		}
-	}
-	return partition
+        }
+        return partition
+}
+
+func (m *ModuleBase) ProductOverlaySpecific() bool {
+	return Bool(m.commonProperties.Product_overlay_specific)
 }
 
 func (m *ModuleBase) Enabled() bool {
@@ -1684,6 +1695,7 @@ func determineModuleKind(m *ModuleBase, ctx blueprint.EarlyModuleContext) module
 	var deviceSpecific = Bool(m.commonProperties.Device_specific)
 	var productSpecific = Bool(m.commonProperties.Product_specific)
 	var systemExtSpecific = Bool(m.commonProperties.System_ext_specific)
+	var productOverlaySpecific = Bool(m.commonProperties.Product_overlay_specific)
 
 	msg := "conflicting value set here"
 	if socSpecific && deviceSpecific {
@@ -1699,12 +1711,12 @@ func determineModuleKind(m *ModuleBase, ctx blueprint.EarlyModuleContext) module
 		}
 	}
 
-	if productSpecific && systemExtSpecific {
+	if productSpecific && (systemExtSpecific || productOverlaySpecific) {
 		ctx.PropertyErrorf("product_specific", "a module cannot be specific to product and system_ext at the same time.")
 		ctx.PropertyErrorf("system_ext_specific", msg)
 	}
 
-	if (socSpecific || deviceSpecific) && (productSpecific || systemExtSpecific) {
+	if (socSpecific || deviceSpecific) && (productSpecific || systemExtSpecific || productOverlaySpecific) {
 		if productSpecific {
 			ctx.PropertyErrorf("product_specific", "a module cannot be specific to SoC or device and product at the same time.")
 		} else {
@@ -1729,6 +1741,8 @@ func determineModuleKind(m *ModuleBase, ctx blueprint.EarlyModuleContext) module
 		return productSpecificModule
 	} else if systemExtSpecific {
 		return systemExtSpecificModule
+	} else if productOverlaySpecific {
+		return productOverlaySpecificModule
 	} else if deviceSpecific {
 		return deviceSpecificModule
 	} else if socSpecific {
@@ -2571,6 +2585,7 @@ func (m *ModuleBase) MakeAsPlatform() {
 	m.commonProperties.Soc_specific = boolPtr(false)
 	m.commonProperties.Product_specific = boolPtr(false)
 	m.commonProperties.System_ext_specific = boolPtr(false)
+        m.commonProperties.Product_overlay_specific = boolPtr(false)
 }
 
 func (m *ModuleBase) MakeAsSystemExt() {
@@ -2579,6 +2594,7 @@ func (m *ModuleBase) MakeAsSystemExt() {
 	m.commonProperties.Soc_specific = boolPtr(false)
 	m.commonProperties.Product_specific = boolPtr(false)
 	m.commonProperties.System_ext_specific = boolPtr(true)
+        m.commonProperties.Product_overlay_specific = boolPtr(false)
 }
 
 // IsNativeBridgeSupported returns true if "native_bridge_supported" is explicitly set as "true"
@@ -2596,6 +2612,10 @@ func (m *moduleContext) InstallInTestcases() bool {
 
 func (m *moduleContext) InstallInSanitizerDir() bool {
 	return m.module.InstallInSanitizerDir()
+}
+
+func (e *earlyModuleContext) ProductOverlaySpecific() bool {
+	return e.kind == productOverlaySpecificModule
 }
 
 func (m *moduleContext) InstallInRamdisk() bool {